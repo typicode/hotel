868591b2cebe47a43b5df37320c5fdfb404893a2
Typicode <typicode@gmail.com>
2015-05-30 17:30:14 +0200

    Fix this

--- src/daemon/public/index.js
+++ src/daemon/public/index.js
@@ -3,11 +3,11 @@ let template = Handlebars.compile(source)
 let socket = io.connect(location.origin)
 
 $('body')
-  .on('click', '.stop', () => {
+  .on('click', '.stop', function () {
     let id = $(this).data('id')
     socket.emit('stop', id)
   })
-  .on('click', '.start', () => {
+  .on('click', '.start', function () {
     let id = $(this).data('id')
     socket.emit('start', id)
   })
