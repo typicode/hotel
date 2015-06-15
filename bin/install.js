d38aa89c979b79239957ee524ddde30515533d94
Typicode <typicode@gmail.com>
2015-06-15 17:06:20 +0200

    Add downgrade-root

--- bin/install.js
+++ bin/install.js
@@ -1,14 +1,19 @@
+var downgradeRoot = require('downgrade-root');
 var autostart = require('../lib/actions/autostart')
 var daemon = require('../lib/actions/daemon')
 
 console.log()
-
+try {
+  downgradeRoot()
+} catch (err) {
+  console.log('  Error: Couldn\'t downgrade permissions')
+  console.log('  Please run: hotel autostart && hotal start')
+  console.log()
+  return
+}
 autostart.create()
-
 console.log()
-
 daemon.start()
-
 console.log()
 console.log('  To uninstall: npm rm -g hotel')
 console.log('')
