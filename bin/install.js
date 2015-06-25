ed0e0f5ae47fe8a8d5dff39c51df3535492d017c
Typicode <typicode@gmail.com>
2015-06-25 18:10:03 +0200

    Update install message

--- bin/install.js
+++ bin/install.js
@@ -1,2 +1,3 @@
-console.log('Hotel is now installed.')
-console.log('Run \'hotel autostart\' and visit \'http://localhost:2000\' to get started.')
+console.log('  Hotel is now installed.')
+console.log('  Run \'hotel start\' and visit \'http://localhost:2000\' to get started.')
+console.log()
