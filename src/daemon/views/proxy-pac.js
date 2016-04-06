44e5303d7971f14d27da37a291a77c33866a1b3b
Erik Nelson <erik.nelson@kroger.com>
2016-04-06 15:43:35 -0400

    Preliminary support for proxies based on http_proxy env variable

--- src/daemon/views/proxy-pac.js
+++ src/daemon/views/proxy-pac.js
@@ -3,7 +3,19 @@ const conf = require('../../conf')
 // Tells the browser to proxy *.dev URLs to hotel
 module.exports =
 `function FindProxyForURL (url, host) {
-  if (dnsDomainIs(host, '.${conf.tld}')) return 'PROXY 127.0.0.1:${conf.port}'
-  return 'DIRECT'
+  var proxy = '${process.env.http_proxy}';
+  if (dnsDomainIs(host, '.${conf.tld}'))
+    return 'PROXY 127.0.0.1:${conf.port}'
+  if (proxy != null && proxy.trim() != "") {
+      if (host == "127.0.0.1"
+        || host == "localhost"
+        || isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0")
+        || isInNet(dnsResolve(host), "192.168.0.0", "255.255.0.0")) {
+        return 'DIRECT'
+      }
+      return 'PROXY ' + proxy
+  } else {
+    return 'DIRECT'
+  }
 }
 `
