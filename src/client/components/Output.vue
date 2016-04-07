390b244d02519f46f56eeba90ca89ab23be3a7f5
David Somers <david@jalada.co.uk>
2016-04-07 12:20:20 +0100

    Support toggling between dark & light console colours

--- src/client/components/Output.vue
+++ src/client/components/Output.vue
@@ -1,13 +1,31 @@
 <template>
-  <div v-on:scroll="onScroll">
-    <div v-for="item in output" track-by="_uid">
-      {{{ item.line }}}
+  <div>
+    <div class="toggle-background">
+      <a
+        class="toggle"
+        v-bind:class="lightsOn() ? 'on' : 'off'"
+        v-on:click.prevent="toggleLights()"
+      >
+        <i
+          class="fa"
+          v-bind:class="lightsOn() ? 'fa-toggle-on' : 'fa-toggle-off'"
+        >
+        </i>
+      </a>
+    </div>
+
+    <div v-on:scroll="onScroll" v-bind:class="lightsOn() ? 'white' : 'black'">
+      <div v-for="item in output" track-by="_uid">
+        {{{ item.line }}}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  props: ['lights'],
+
   data() {
     return {
       scroll: true,
@@ -36,6 +54,14 @@ export default {
     onScroll(event) {
       var element = event.target
       this.scroll = element.scrollHeight - element.scrollTop === element.clientHeight
+    },
+
+    lightsOn() {
+      return this.lights;
+    },
+
+    toggleLights() {
+      this.$dispatch('toggle-lights');
     }
   }
 }
