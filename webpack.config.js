ba92989b52bf72c2f3cbd1bdc365b92a911c23cc
typicode <typicode@gmail.com>
2017-02-11 01:31:44 +0100

    Update webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -14,20 +14,22 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        loader: 'babel-loader'
       },
       {
         test: /\.json$/,
-        loader: 'json'
+        loader: 'json-loader'
       },
       {
         test: /\.vue$/, // a regex for matching all files that end in `.vue`
-        loader: 'vue'   // loader to use for matched files
+        loader: 'vue-loader'   // loader to use for matched files
       }
     ]
   },
   // https://github.com/vuejs/vue/wiki/Vue-2.0-RC-Starter-Resources#standalone-vs-runtime-builds
   resolve: {
-    alias: {vue: 'vue/dist/vue.js'}
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
   }
 }
