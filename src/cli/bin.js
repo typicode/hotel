30e4b0d4f70db8af0f95d834ecac8628b497a46b
yeskunall <yeskunall96@gmail.com>
2017-07-17 09:47:17 -0400

    use please-upgrade-node instead (#204)

--- src/cli/bin.js
+++ src/cli/bin.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-const updateNotifier = require('update-notifier')
+const upgradeNode = require('please-upgrade-node')
 const sudoBlock = require('sudo-block')
 const pkg = require('../../package.json')
 
 sudoBlock('\nShould not be run as root, please retry without sudo.\n')
-updateNotifier({ pkg }).notify()
+upgradeNode({ pkg })
 require('./')(process.argv)
