b345df2362a87bdc7fe56dac1d9279b3d3a17b7c
Daniel Perez Alvarez <danielpza@protonmail.com>
2019-02-15 02:09:06 -0500

    Add links

--- src/app/formatter.ts
+++ src/app/formatter.ts
@@ -2,8 +2,14 @@ import * as ansi2HTML from 'ansi2html'
 import * as escapeHTML from 'escape-html'
 import { IMonitor } from './Store'
 
+const URL_REGEXP = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+
 function blankLine(val: string) {
   return val.trim() === '' ? '&nbsp;' : val
+}
+
+function addLinks(str: string) {
+  return str.replace(URL_REGEXP, (match) => `<a href=${match}>${match}</a>`)
 }
 
 export function formatLines(str: string): string[] {
@@ -12,13 +18,14 @@ export function formatLines(str: string): string[] {
     .split('\n')
     .map(escapeHTML)
     .map(blankLine)
+    .map(addLinks)
     .map(ansi2HTML)
 }
 
 export function statusTitle(monitor: IMonitor) {
   return monitor.pid
     ? `PID ${monitor.pid}\nStarted since ${new Date(
-        monitor.started
-      ).toLocaleString()}`
+      monitor.started
+    ).toLocaleString()}`
     : ''
 }
